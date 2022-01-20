927cd1995ba363dc34dbcf493f54c66d1e9e2cd5
mo256man <48610559+mo256man@users.noreply.github.com>
2022-01-20 17:37:15 +0900

    Add files via upload

--- roomba_race_origin.py
+++ roomba_race_origin.py
@@ -1,24 +1,15 @@
-import create
+from pyroombaadapter import PyRoombaAdapter
+#import create
 import time
 import datetime
 import sys
 import random
 import serial.tools.list_ports
+import math
 
-"""
-ルンバを動かす
-    robot.go(cm_per_sec=, deg_per_sec)
-        cm_per_sec…
-        deg_per_sec…
-
-
-    robot.stop()
-        ルンバを停止する
-        内部的にrobot.go(0,0)と同じ
-
-    
-
-"""
+ROOMBA_PORT = get_roomba_port()                             # ルンバのポートを取得
+robot = PyRoombaAdapter(ROOMBA_PORT)                        # そのポートでルンバを操作する
+robot.change_mode_to_safe()    
 
 
 def get_roomba_port():
@@ -30,36 +21,57 @@ def get_roomba_port():
     for comport in comports:                                # 取得した個々のポートデータにおいて
         if "/dev/ttyUSB" in comport.device:                 # "/dev/ttyUSB" という文字列がデバイスにあったら
             port = comport.device                           # それがルンバが接続されているポート
+            ser = serial.Serial(port)
+            ser.close()
+                                                            
     
     if port == "":                                          # ルンバが見つからなかったら
-        print("ルンバが見つかりません")                           # メッセージを表示して
+        print("ルンバが見つかりません")                     # メッセージを表示して
         sys.exit()                                          # プログラムを中断する
     return port
-    
 
 
-def main():
-    ROOMBA_PORT = get_roomba_port()                         # ルンバのポートを取得
-    robot = create.Create(ROOMBA_PORT)                      # そのポートでルンバを操作する
+def stop():
+    robot.move(0, 0)  # stop
     
-    robot._start()                                          # ルンバ起動
-    robot.toSafeMode()                                      # セーフモードにする
-#    robot.toFullMode()                                     # 持ち上げても止まらないフルモードにするにはこちら
-    
-#    robot.printSensors() 
 
-    while True:                                             # 無限ループ
+def go(distance):
+    """
+    go for x m
+    """
+    go_speed = 0.2              # m/sec
+    if distance < 0:
+        go_speed = -go_speed
+    robot.move(go_speed, 0)  # go straight
+    wait_time = distance / go_speed         # sec
+    time.sleep(wait_time)
+    stop()
+
+
+
+def turn(angle_degree):
+    """
+    turn x degree
+    """
+    angle_degree = angle_degree/1.21
+    turn_speed = 2 * math.pi / 360 * 45                # rad/sec
+    if angle_degree < 0:
+        turn_speed = -turn_speed
         
-        if robot.getMode() != create.SAFE_MODE:             # セーフモードでなくなったら
-            robot.stop()                                    # ロボットを停止して
-            sys.exit()                                      # プログラムを中断する
-        
-        # ここから下にルンバで迷路を脱出するコードを書く
-            
-         
-
-        time.sleep(0.1)                                     # 0.1秒停止する
+    angle_radians = 2 * math.pi / 360 * angle_degree
+    robot.move(0, turn_speed)  # go straight
+    wait_time = angle_radians / turn_speed
+    time.sleep(wait_time)
+    stop()
 
 
-if __name__ == "__main__":
-    main()
+
+  
+# ここから下にルンバで迷路を脱出するコードを書く
+go(0.5)
+turn(180)
+go(0.5)
+turn(-180)
+
+#
+del robot
